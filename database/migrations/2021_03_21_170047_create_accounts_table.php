51c54637667ea555ff2ec5553d57329bee5b6cd1
Daniel Tremura <tremuradaniel@gmail.com>
2021-03-21 19:12:12 +0200

    migrations for accounts

---
+++ database/migrations/2021_03_21_170047_create_accounts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('currency_id')->constrained('currencies');
+            $table->foreignId('account_type_id')->constrained('account_types');
+            $table->decimal('amount', 15, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
