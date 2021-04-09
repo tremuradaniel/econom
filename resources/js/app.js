cd4b3f1f6925481371ac76b775dbf610ba930f6f
Daniel Tremura <tremuradaniel@gmail.com>
2021-04-09 08:59:39 +0300

    fixed route issue

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,10 @@ import { createApp, h } from 'vue'
 import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress'
 import mitt from 'mitt'
-import route from 'ziggy'
 
 const emitter = mitt()
 const el = document.getElementById('app')
+const route = window.route
 
 require('./bootstrap')
 
